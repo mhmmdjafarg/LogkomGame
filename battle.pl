7d2bfc477e02863a77af25939bf0210fd931d6d6
Muhammad Fahmi Alamsyah <49779495+tararahuuw@users.noreply.github.com>
2020-11-23 16:00:49 +0700

    add battle and main

---
+++ battle.pl
@@ -0,0 +1,98 @@
+:- include('player.pl').
+:- include('dungeon.pl').
+
+/* fight, run, gagalrun, attack, skill, itung turn, levelup */
+
+:- dynamic(inBattle/1).
+:- dynamic(enemy/1). %menandakan musuhnya apa
+:- dynamic(totalTurn/1). % menghitung jumlah turn pemain
+
+:- discontiguous(decide/0).
+:- discontiguous(run/0).
+:- discontiguous(fight/0).
+:- discontiguous(cannotRun/0).
+:- discontiguous(goRun/0).
+:- discontiguous(updateHPPlayer/2).
+:- discontiguous(updateHPMonster/2).
+
+
+/* Pilih random enemy */
+randomenemy :-
+    % random untuk wolf slime goblin
+    random(1,3, Id),
+    idDungeon(EnemyType,Id),
+    asserta(enemy(EnemyType)),
+    printEnemyStats(EnemyType), !.
+
+
+printEnemyStats(Enemy) :-
+    level(Enemy, Level),
+    attack(Enemy, Att),
+    defence(Enemy, Def),
+    baseHp(Enemy, BaseHp),
+    write(Enemy), write('appears'), nl,
+    write('Level : '), write(Level), nl,
+    write('Attack : '), write(Att), nl,
+    write('Defence : '), write(Def), nl,
+    write('base Hp : '), write(BaseHp), nl, !.
+
+
+/* pertama kali bertemu dungeon */
+decide :-
+    asserta(inBattle(1)).
+    write('Beast creature appears, prepare yourself!'), nl,
+    randomenemy,
+    write('fight or run? '), nl.
+    
+
+% kondisi sedang tidak bermain %
+run :-
+	\+playing(_), % ini ada di player.pl atau main.pl
+    write('The game not started yet, type \'start.\' first'), nl,!.
+
+% kondisi tidak sedang dalam battle
+run :-
+    playing(_)
+	 \+inBattle(_),
+    write('You are not in battle, run for what?'), nl,!.
+  
+run :-
+    inBattle(_),
+    random(1,5,Number),
+    (Number =< 2 -> cannotRun; goRun).
+
+cannotRun :-
+    write('Too slow, he runs at you!'), nl,
+    fight.
+
+goRun :-
+    write('Nice move, you escape!'), nl,
+    retractall(inBattle(_)),
+    retractall(enemy(_)).
+
+
+% fight
+fight :-
+    \+playing(_),
+    write('Its not started yet').
+
+fight :-
+    playing(_), \+inBattle(_),
+    write('Are you mad? Go somewhere else to find monster!'), nl.
+
+
+updateHPPlayer (Player,Monster) :-
+    attack(Monster,X),
+    healthPlayer(Player, Y),
+    TempHpPlayer is Y - X,
+    retract(healthPlayer(Player,_)),
+
+    assertz(healthPlayer(Player, TempHp)),!.
+
+updateHPMonster (Player,Monster) :-
+    damage(Player, X),
+    health(Monster,Y),
+    TempHpMonster is Y - X,
+    retract(health(Monster,_)),
+    
+    assertz(health(Monster, TempHpMonster)),!.
---
+++ main.pl
@@ -0,0 +1,31 @@
+:- include('map.pl').
+
+:- dynamic(playing/1). % untuk status sedang bermain atau tidak
+
+start :-
+    playing(_),
+    write('The game has already started!'),nl.
+
+start :-
+    asserta(playing(1)),
+    write('______                _       _          ___      _                 _'),nl,                  
+    write('| ___ \\              | |     ( )        / _ \\    | |               | |'),nl,                 
+    write('| |_/ /__ _ _ __   __| |_   _|/ ___    / /_\\ \\ __| |_   _____ _ __ | |_ _   _ _ __ ___'),nl, 
+    write('|    // _  |  _ \\ / _  | | | | / __|   |  _  |/ _  \\ \\ / / _ \\  _ \\| __| | | |  __/ _ \\'),nl,
+    write('| |\\ \\ (_| | | | | (_| | |_| | \\__ \\   | | | | (_| |\\ V /  __/ | | | |_| |_| | | |  __/'),nl,
+    write('\\_| \\_\\__,_|_| |_|\\__,_|\\__, | |___/   \\_| |_/\\__,_| \\_/ \\___|_| |_|\\__|\\__,_|_|  \\___|'),nl,
+    write('                         __/ |'),nl,                                                         
+    write('                        |___/'),nl,nl,
+    writelore,
+    mulai,
+    map.
+
+writelore :-
+	write('Anda adalah seorang pengembara yang berasal dari kota bawah tanah, Dangarnon, yang dikhianati oleh rekan pengembara anda.'),nl,
+	write('Rekan pengembara anda pergi meninggalkan anda, pulang sendirian sambil membawa kunci untuk kembali ke Dangarnon. Anda'),nl,
+	write('ditinggalkan sendirian di dunia atas, permukaan bumi. Saat anda terbangun, anda disambut oleh baiknya warga bumi.'),nl,
+	write('Anda memilih untuk membalas kebaikan mereka terlebih dahulu sebelum berusaha untuk kembali ke Dangarnon, anda membantu'),nl,
+	write('melawan parasit-parasit yang meresahkan warga. Anda harus memperkuat diri karena pintu gerbang ke Dangarnon diawasi oleh'),nl,
+	write('teman pengembara anda yang tidak ingin anda kembali ke Dangarnon, yang rekan pengembara anda tidak ketahui adalah penghuni'),nl,
+	write('dunia atas juga memiliki kunci untuk pergi ke dunia bawah tanah, bisakah anda kembali ke Dangarnon? dan bisakah anda mencari'),nl,
+	write('tahu bagaimana penghuni dunia atas bisa memiliki kunci ke Dangarnon?'),nl,nl.
