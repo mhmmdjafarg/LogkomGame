4bed90764c7b49ec1fcbc119bec6d62deaa4fe77
Muhammad Fahmi Alamsyah <49779495+tararahuuw@users.noreply.github.com>
2020-11-24 08:58:54 +0700

    add equip armor

--- equipment.pl
+++ equipment.pl
@@ -8,7 +8,9 @@ initEquipment :-
     char(Karakter),
     (Karakter =:= swordsman -> FirstWeapon is common_sword; Karakter =:= archer -> FirstWeapon is common_bow; Karakter =:= ninja -> FirstWeapon is shuriken),
     damageweapon(FirstWeapon, Val),
+
     assertz(equipment(weapon,FirstWeapon, Val)),!.
+
     (Karakter =:= swordsman -> FirstArmor is common_armor; Karakter =:= archer -> FirstArmor is common_armor; Karakter =:= ninja -> FirstArmor is common_armor),
     kerasarmor(FirstArmor, ValArmor),
     assertz(equipment(armor,FirstArmor, ValArmor)),!.
@@ -29,7 +31,23 @@ useEquipment(NamaEquipment) :-
     assertz(inventory(X1,NamaEquipment)),
     retractall(equipment(weapon, _, _)),
     damageweapon(NamaEquipment, Val),
-    assertz(equipment(weapon, NamaEquipment, Val)),!.
+    assertz(equipment(weapon, NamaEquipment, Val)),!.; 
+    %else if
+    inventory(X, NamaEquipment),
+    X > 0,
+    char(Karakter),
+    typearmor(Karakter, NamaEquipment),
+    equipment(armor,UsedEquipment, _ ),
+    inventory(Y, UsedEquipment),
+    Y1 is Y + 1,
+    retractall(inventory(_,UsedEquipment)),
+    assertz(inventory(Y1, UsedEquipment)),
+    X1 is X - 1,
+    retractall(inventory(_,NamaEquipment)),
+    assertz(inventory(X1,NamaEquipment)),
+    retractall(equipment(armor, _, _)),
+    kerasarmor(NamaEquipment, ValArmor),
+    assertz(equipment(armor, NamaEquipment, ValArmor)),!.
     
 % cek untuk weapon
 useEquipment(NamaEquipment) :-
